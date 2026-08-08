b777d0fba4f598028469ecc00780530555aa7003
generalio <1487144524@qq.com>
2026-08-09 01:58:43 +0800

    fix(ios): 兼容新版 WKUIDelegate 文件选择回调

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,6 +12,6 @@ dependencies {
   implementation("com.android.tools.build:gradle:8.7.3")
   // 使用基础插件接入 Central Portal 和 GPG 签名，发布物仍由本工程约定插件手动注册。
   implementation("com.vanniktech:gradle-maven-publish-plugin:0.34.0")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
   implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.17.0")
 }
--- webview-ios/build.gradle.kts
+++ webview-ios/build.gradle.kts
@@ -1,17 +1,8 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
   id("multiweb.ios-library")
 }
 
 kotlin {
-  targets.withType<KotlinNativeTarget>().configureEach {
-    compilations.getByName("main").cinterops.create("multiWebFileChooser") {
-      definitionFile.set(project.file("src/nativeInterop/cinterop/MultiWebFileChooser.def"))
-      compilerOpts("-I${project.projectDir}/src/nativeInterop/cinterop")
-    }
-  }
-
   sourceSets {
     iosMain.dependencies {
       api(project(":webview-api"))
